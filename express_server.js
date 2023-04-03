2a85dbc0e2b8423c8fc6b9f4362988e1e222cdb3
titanium-cmd <nkansahisaac41@gmail.com>
2023-04-03 18:55:39 +0000

    sending default message

--- express_server.js
+++ express_server.js
@@ -4,15 +4,16 @@ const mongoose = require('mongoose');
 const usersRoutes = require('./routes/usersRoutes');
 
 mongoose.connect(process.env.MONGO_URL)
-.then(()=>{
-  console.log('DB is now connected');
-})
-.catch((error)=>{
-  console.log('DB not connected: ', error.message);
-});
+  .then(() => {
+    console.log('DB is now connected');
+  })
+  .catch((error) => {
+    console.log('DB not connected: ', error.message);
+  });
 
 const server = express();
 server.use(express.json());
+server.get('/', (_, res) => res.status(200).json({ success: true, message: 'Welcome to My Express Server' }));
 server.use('/users', usersRoutes);
 
-server.listen(3001);
+server.listen(8080);
